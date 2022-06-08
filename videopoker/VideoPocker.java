af46fdab16b47979751ee19439f7e5f562566e48
RuiFerreira <rui_s.ferreira@hotmail.com>
2022-06-08 18:34:09 +0100

    updated VideoPocker.java

--- videopoker/VideoPocker.java
+++ videopoker/VideoPocker.java
@@ -2,8 +2,8 @@ package videopoker;
 
 public class VideoPocker {
 
-  public VideoPocker() {
-    super():
-    // TODO Auto-generated constructor stub
-  }
+	public VideoPocker() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
