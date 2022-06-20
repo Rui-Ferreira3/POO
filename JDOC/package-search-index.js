fd899964f4449db55778405fdb26814128431c84
RicardoFebra <ricardo.febra@gmail.com>
2022-06-20 09:47:58 +0100

    lilazes

--- JDOC/package-search-index.js
+++ JDOC/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"videopoker"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"videopoker"}];updateSearchResults();
